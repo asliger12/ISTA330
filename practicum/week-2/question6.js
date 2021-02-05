9332f88b69995beb1878bd24e1715883a00f7a8e
Araya Sliger <77595860+asliger12@users.noreply.github.com>
2021-02-04 21:58:53 -0700

    question 6

--- practicum/week-2/question6.js
+++ practicum/week-2/question6.js
@@ -6,5 +6,15 @@ output: 3
 */
 
 var median = function(input) {
-
+    var sorted = input.sort();
+    var result = 0;
+    var l = sorted.length
+    if(l % 2 == 1){
+        result = sorted[Math.round((l-1)/2)]
+    } else {
+        var a = sorted[l/2]
+        var b = sorted[(l/2)-1]
+        result = (a+b)/2
+    }
+    return result;
 };
