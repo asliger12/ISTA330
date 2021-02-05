c620fc5272545aca1195ae048f2fae826346c55b
Araya Sliger <77595860+asliger12@users.noreply.github.com>
2021-02-05 09:46:17 -0700

    question 9

--- practicum/week-2/question9.js
+++ practicum/week-2/question9.js
@@ -9,5 +9,16 @@ output: 'hello'
 */
 
 var suffleString = function(input, shuffleIndices) {
-
+    var newString = input.split('')
+    for (var i = 1; i < shuffleIndices.length; i++)
+        for (var j = 0; j < i; j++)
+            if (shuffleIndices[i] < shuffleIndices[j]) {
+                var x = shuffleIndices[i];
+                var y = newString[i];
+                shuffleIndices[i] = shuffleIndices[j];
+                newString[i] = newString[j];
+                shuffleIndices[j] = x;
+                newString[j] = y;
+        }
+    return newString.join('');
 };
