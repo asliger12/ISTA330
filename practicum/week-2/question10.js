b9cf4c3a7e615c852ca36ae31f586aa2e2cdc109
Araya Sliger <77595860+asliger12@users.noreply.github.com>
2021-02-05 09:28:15 -0700

    question 10

--- practicum/week-2/question10.js
+++ practicum/week-2/question10.js
@@ -8,5 +8,9 @@ output: 7 - 2 = 5
 */
 
 var maxMinusMin = function(n) {
-
+    var nums = n.toString().split('');
+    var max = Math.max.apply(null, nums)
+    var min = Math.min.apply(null, nums)
+    var result = parseInt(max-min)
+    return result;
 };
