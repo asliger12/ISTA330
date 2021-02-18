c476c979d4a639892a41b4958cee9cad2900da64
Araya Sliger <77595860+asliger12@users.noreply.github.com>
2021-02-18 15:59:35 -0700

    question 4

--- practicum/week-3/question4.js
+++ practicum/week-3/question4.js
@@ -13,5 +13,13 @@ Example:
 */
 
 var hasDuplicates = function(input) {
-
+    for (var i=0; i < input.length; i++){
+        var toCompare = input[i]
+        for (var j=i+1; j <= input.length; j++){
+            if (toCompare===input[j]){
+                return true;
+            }
+        }
+    }
+    return false;
 };
