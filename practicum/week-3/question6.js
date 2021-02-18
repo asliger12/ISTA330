8cdace7e908d631ee4d6b6ebb08f6c388b7c5a54
Araya Sliger <77595860+asliger12@users.noreply.github.com>
2021-02-18 16:19:28 -0700

    question 6

--- practicum/week-3/question6.js
+++ practicum/week-3/question6.js
@@ -12,5 +12,19 @@ output: 2
 */
 
 var m_element = function(input) {
-
+    var n = input.length / 2
+    var count = 1
+    for (var i=0; i < input.length; i++){
+        for (var j=i+1; j <= input.length; j++){
+            if (input[i]===input[j]){
+                count ++;
+            }
+        }   
+        if (count > n){
+            return input[i];
+        }
+    }
+    return -1;
 };
+
+console.log(m_element([1, 2, 3, 4, 5, 5, 2]))
