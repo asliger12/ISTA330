59c515e68bbfea26cb71cc8a63bc5920cb57d5d6
Araya Sliger <77595860+asliger12@users.noreply.github.com>
2021-02-19 10:10:01 -0700

    question 7

--- practicum/week-3/question7.js
+++ practicum/week-3/question7.js
@@ -12,5 +12,12 @@ output: [[1,4], [2,5], [3,6]]
 */
 
 var transpose = function(M) {
-
+    newArray = []
+    for (var i = 0; i < M[0].length; i++) { 
+        newArray.push([]);
+        for (var j = 0; j < M.length; j++) { 
+            newArray[i].push(i[j])
+        }
+    } 
+    return newArray;
 };
